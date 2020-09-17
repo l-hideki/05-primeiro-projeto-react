9ee9b2634bd79b34ceaf6a7f56642c059471ed43
Lucas Hideki <ls_hdk@protonmail.com>
2020-09-17 17:10:31 -0300

    Estilizando-dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,56 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repósitório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/53327704?s=400&u=3733bc7c0d41a45aa2c6a0930696a43da2aae998&v=4"
+            alt="Lucas Hideki"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p> teste </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/53327704?s=400&u=3733bc7c0d41a45aa2c6a0930696a43da2aae998&v=4"
+            alt="Lucas Hideki"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p> teste </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/53327704?s=400&u=3733bc7c0d41a45aa2c6a0930696a43da2aae998&v=4"
+            alt="Lucas Hideki"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p> teste </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
